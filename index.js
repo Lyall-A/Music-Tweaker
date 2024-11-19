2d8eaeed527e0e570a9c76d642747282fc2f2c0b
Lyall <me@lyall.lol>
2024-11-19 17:51:52 +0000

    presets and shit

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ const argOptions = [
     { long: ["phaser"], description: "Add phaser effect", bool: true },
     { long: ["pitch"], description: "Pitch multiplier, doesn't change speed" },
     { long: ["bass"], description: "Bass boost" },
+    { long: ["tempo"], description: "Tempo" },
     { long: ["pulsate"], description: "Pulsate" },
     { name: "audioFilters", short: ["af"], long: ["audio_filters"], description: "Add more audio filters" },
     { short: ["hp"], long: ["highpass"], description: "High-pass" },
     { short: ["lp"], long: ["lowpass"], description: "Low-pass" },
     { short: ["v", "vol"], long: ["volume"], description: "Change volume" },
     { short: ["b:a"], long: ["bitrate"], description: "Bitrate" },
+    { short: ["p"], long: ["preset"], description: "Preset" },
     { short: ["c:a"], long: ["codec"], description: "Output codec" },
     { short: ["f"], long: ["format"], description: "Output format" },
     { short: ["o"], long: ["output"], description: "Output path" },
@@ -57,43 +59,48 @@ for (const options of argOptions) {
 };
 const cliArgs = Object.fromEntries(argOptions.map(i => ([i.name || i.long?.[0] || i.short?.[0], i.value])));
 
+const presets = require("./presets.json");
+const preset = cliArgs.preset ? presets.find(i => i.id === cliArgs.preset || i.name === cliArgs.preset) : null;
+if (cliArgs.preset && !preset) return console.log(`Couldn't find preset '${cliArgs.preset}!`);
+
 // idk just functions to make things easier
 const getSampleRate = (input) => ffprobe(input).then(i => parseInt(i.streams[0].sample_rate));
 const getFormat = (input) => ffprobe(input).then(i => i.format.format_name);
 
 // main
 (async function main() {
-    const inputPath = cliArgs.input;
+    const inputPath = getOption("input");
     const inputExtName = path.extname(inputPath).substring(1);
 
     if (!fs.existsSync(inputPath)) return console.log(`Input file '${inputPath}' doesn't exist`);
     const sampleRate = await getSampleRate(inputPath);
-    const formats = await getFormat(inputPath).then(i => i.split(","))
+    const formats = await getFormat(inputPath).then(i => i.split(","));
     
-    const outputFormat = cliArgs.format || (formats.length ? formats.includes(inputExtName) ? inputExtName : formats[0] : formats[0]); // ffprobe can return multiple formats, look for file format or use the first one
-    const outputPath = cliArgs.output || path.join(__dirname, `${path.basename(inputPath, `.${inputExtName}`)}.${outputFormat}`);
+    const outputFormat = getOption("format") || (formats.length ? formats.includes(inputExtName) ? inputExtName : formats[0] : formats[0]); // ffprobe can return multiple formats, look for file format or use the first one
+    const outputPath = getOption("output") || path.join(__dirname, `${path.basename(inputPath, `.${inputExtName}`)}.${outputFormat}`);
     const audioFilters = [];
     const args = [];
 
     // audio filters
-    if (cliArgs.pitch) audioFilters.push(`asetrate=${sampleRate}*${cliArgs.pitch}`);
-    if (cliArgs.volume) audioFilters.push(`volume=${cliArgs.volume / 100}`);
-    if (cliArgs.nightcore) audioFilters.push(`asetrate=${sampleRate}*1.25,aresample=${sampleRate}`);
-    if (cliArgs.slowed) audioFilters.push(`asetrate=${sampleRate}*0.9`);
-    if (cliArgs.bass) audioFilters.push(`bass=g=${cliArgs.bass}`);
-    if (cliArgs.reverse) audioFilters.push(`areverse`);
-    if (cliArgs.highpass) audioFilters.push(`highpass=f=${cliArgs.highpass}`);
-    if (cliArgs.lowpass) audioFilters.push(`lowpass=f=${cliArgs.lowpass}`);
-    if (cliArgs.pulsate) audioFilters.push(`apulsator=hz=${cliArgs.pulsate}`);
-    if (cliArgs.noiseReduction) audioFilters.push(`afftdn`);
-    if (cliArgs.flanger) audioFilters.push(`flanger`);
-    if (cliArgs.phaser) audioFilters.push(`aphaser`);
-    if (cliArgs.audioFilters) audioFilters.push(cliArgs.audioFilters);
+    if (getOption("pitch")) audioFilters.push(`asetrate=${sampleRate}*${getOption("pitch")}`);
+    if (getOption("volume")) audioFilters.push(`volume=${getOption("volume") / 100}`);
+    if (getOption("nightcore")) audioFilters.push(`asetrate=${sampleRate}*1.25,aresample=${sampleRate}`);
+    if (getOption("slowed")) audioFilters.push(`asetrate=${sampleRate}*0.9`);
+    if (getOption("bass")) audioFilters.push(`bass=g=${getOption("bass")}`);
+    if (getOption("tempo")) audioFilters.push(`atempo=${getOption("tempo")}`);
+    if (getOption("reverse")) audioFilters.push(`areverse`);
+    if (getOption("highpass")) audioFilters.push(`highpass=f=${getOption("highpass")}`);
+    if (getOption("lowpass")) audioFilters.push(`lowpass=f=${getOption("lowpass")}`);
+    if (getOption("pulsate")) audioFilters.push(`apulsator=hz=${getOption("pulsate")}`);
+    if (getOption("noiseReduction")) audioFilters.push(`afftdn`);
+    if (getOption("flanger")) audioFilters.push(`flanger`);
+    if (getOption("phaser")) audioFilters.push(`aphaser`);
+    if (getOption("audioFilters")) audioFilters.push(getOption("audioFilters"));
     
     // other args
     if (audioFilters.length) args.push("-af", audioFilters.join(","));
-    if (cliArgs.bitrate) args.push("-b:a", cliArgs.bitrate);
-    if (cliArgs.codec) args.push("-c:a", cliArgs.codec);
+    if (getOption("bitrate")) args.push("-b:a", getOption("bitrate"));
+    if (getOption("codec")) args.push("-c:a", getOption("codec"));
     args.push("-f", outputFormat); // format is needed since outputting to pipe
 
     console.log(`Using FFmpeg args '${args.join(" ")}'`);
@@ -106,11 +113,15 @@ const getFormat = (input) => ffprobe(input).then(i => i.format.format_name);
     ffplay(outputPath, ["-autoexit"]); // funny testing
 })();
 
+function getOption(key) {
+    return cliArgs[key] || preset?.options[key];
+}
+
 // run ffmpeg
 function ffmpeg(input, args = [], inputBuffer) {
     args = ["-i", inputBuffer ? "-" : input, ...args, "-"].filter((value, index, array) => array.indexOf(value) === index);
     return new Promise((resolve, reject) => {
-        const ffmpegProcess = cp.spawn(cliArgs.ffmpeg, args);
+        const ffmpegProcess = cp.spawn(getOption("ffmpeg"), args);
         if (inputBuffer) ffmpegProcess.stdin.write(inputBuffer);
         const data = [];
         const logData = [];
@@ -129,7 +140,7 @@ function ffmpeg(input, args = [], inputBuffer) {
 function ffprobe(input, args = [], inputBuffer) {
     args = ["-i", inputBuffer ? "-" : input, ...args, "-print_format", "json", "-show_format", "-show_streams"].filter((value, index, array) => array.indexOf(value) === index);
     return new Promise((resolve, reject) => {
-        const ffprobeProcess = cp.spawn(cliArgs.ffprobe, args);
+        const ffprobeProcess = cp.spawn(getOption("ffprobe"), args);
         if (inputBuffer) ffprobeProcess.stdin.write(inputBuffer);
         const data = [];
         ffprobeProcess.stdout.on("data", i => data.push(i));
@@ -149,9 +160,11 @@ function ffprobe(input, args = [], inputBuffer) {
 function ffplay(input, args = [], inputBuffer) {
     args = ["-i", inputBuffer ? "-" : input, ...args].filter((value, index, array) => array.indexOf(value) === index);
     return new Promise((resolve, reject) => {
-        const ffplayProcess = cp.spawn("/bin/ffplay", args, { detached: true, stdio: "ignore" });
+        const ffplayProcess = cp.spawn("/bin/ffplay", args);
+        console.log(`Playing '${input}' with FFplay`);
+        // const ffplayProcess = cp.spawn("/bin/ffplay", args, { detached: true, stdio: "ignore" });
         if (inputBuffer) ffprobeProcess.stdin.write(inputBuffer);
-        // ffplayProcess.stderr.on("data", i => process.stdout.write(i));
+        ffplayProcess.stderr.on("data", i => process.stdout.write(i));
         ffplayProcess.on("exit", code => {
             if (code > 0) return reject(`FFplay exited with code ${code}`);
             resolve();
